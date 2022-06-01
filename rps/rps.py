d868b8bbf72ce65074297e055cb2ea078a7b9fe7
Redx <noah13victor@gmail.com>
2022-06-01 23:26:38 +0100

    Update rps.py

--- rps/rps.py
+++ rps/rps.py
@@ -28,38 +28,3 @@ while True:
     if play_again.lower() != "y":
         break
 
-
-
-
-# import random
-
-# while True:
-#     user_action = input("Enter a choice, R- rock, P- paper, S- scissors:\n")
-
-#     possible_actions = ["rock", "paper", "scissors"]
-#     computer_action = random.choice(possible_actions)
-
-#     if user_action == computer_action:
-#         print(f"Both players selected {user_action}. It's a tie!")
-
-#     elif user_action == "rock" or "R":
-#         if computer_action == "scissors":
-#             print("Computer chose scissors\nRock smashes scissors! You win!")
-#         else:
-#             print("Computer chose paper\nPaper covers rock! You lose.")
-
-#     elif user_action == "paper" or "P":
-#         if computer_action == "rock":
-#             print("Computer chose rock\nPaper covers rock! You win!")
-#         else:
-#             print("Computer chose scissors\nScissors cuts paper! You lose.")
-
-#     elif user_action == "scissors" or "S":
-#         if computer_action == "paper":
-#             print("Computer chose paper\nScissors cuts paper! You win!")
-#         else:
-#             print("Computer chose rock\nRock smashes scissors! You lose.")
-
-#     play_again = input("Play again? (y/n): ")
-#     if play_again.lower() != "y":
-#         break
