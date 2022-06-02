f58bdfec92454feb5845c43ee998dceaea53af38
Redx <noah13victor@gmail.com>
2022-06-02 05:14:48 +0100

    Update main.py

--- rps/main.py
+++ rps/main.py
@@ -1,29 +1,29 @@
 import random
 
 while True:
-    user_action = input("Enter a choice ('R' for 'rock', 'P' for 'paper','S' for 'scissors'): ")
-    possible_actions = ["rock", "paper", "scissors"]
-    computer_action = random.choice(possible_actions)
-    print(f"\nYou chose {user_action}, CPU chose {computer_action}.\n")
+    user_choice = input("Enter a choice ('R' for 'rock', 'P' for 'paper','S' for 'scissors'): ")
+    available_options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(available_options)
+    print(f"\nYou chose {user_choice}, CPU chose {computer_choice}.\n")
 
-    if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "rock" or "R":
-        if computer_action == "scissors":
+    if user_choice == computer_choice:
+        print(f"Both players selected {user_choice}. It's a tie!")
+    elif user_choice == "rock" or "R":
+        if computer_choice == "scissors":
             print("Rock smashes scissors! You win!")
         else:
             print("Paper covers rock! You lose.")
-    elif user_action == "paper" or "P":
-        if computer_action == "rock":
+    elif user_choice == "paper" or "P":
+        if computer_choice == "rock":
             print("Paper covers rock! You win!")
         else:
             print("Scissors cuts paper! You lose.")
-    elif user_action == "scissors" or "S":
-        if computer_action == "paper":
+    elif user_choice == "scissors" or "S":
+        if computer_choice == "paper":
             print("Scissors cuts paper! You win!")
         else:
             print("Rock smashes scissors! You lose.")
 
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
+    repeat = input("Play again? (y/n): ")
+    if repeat.lower() != "y":
         break
